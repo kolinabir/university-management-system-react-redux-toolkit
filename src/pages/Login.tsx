fd1f6572f4510d6f524d223267c2cc595f6cec6b
kolinabir <knkolin9@gmail.com>
2024-07-08 19:39:18 +0600

    chore: Update dependencies and add redux-persist for state persistence

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,98 @@
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Input, Button, Card, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { jwtDecode } from "jwt-decode";
+
+const { Title } = Typography;
+
+const verifyToken = (token: string) => {
+  return jwtDecode(token);
+};
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const dispatch = useAppDispatch();
+
+  const [login, { error }] = useLoginMutation();
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(
+      setUser({
+        user: user,
+        token: res.data.accessToken,
+      })
+    );
+  };
+
+  return (
+    <Card style={{ maxWidth: 400, margin: "50px auto" }}>
+      <Title level={2} style={{ textAlign: "center", marginBottom: 24 }}>
+        Login
+      </Title>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="id"
+          control={control}
+          rules={{ required: "ID is required" }}
+          render={({ field }) => (
+            <Input
+              {...field}
+              prefix={<UserOutlined />}
+              placeholder="ID"
+              style={{ marginBottom: 16 }}
+            />
+          )}
+        />
+        {errors.id && (
+          <p style={{ color: "red", marginTop: -8, marginBottom: 8 }}>
+            {errors.id.message}
+          </p>
+        )}
+
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: "Password is required" }}
+          render={({ field }) => (
+            <Input.Password
+              {...field}
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              style={{ marginBottom: 16 }}
+            />
+          )}
+        />
+        {errors.password && (
+          <p style={{ color: "red", marginTop: -8, marginBottom: 8 }}>
+            {errors.password.message}
+          </p>
+        )}
+
+        <Button type="primary" htmlType="submit" block>
+          Log in
+        </Button>
+      </form>
+    </Card>
+  );
 };
 
 export default Login;
--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import { baseApi } from "../api/baseApi";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
